e05caba911cfec6fcef46d91859573da7e490014
Ryan Yeh <ryan.yeh@ibm.com>
2018-12-06 15:36:22 -0500

    module 45

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,7 +4,7 @@ class QuizQuestionButton extends Component {
   render() {
     return (
       <li>
-        <button> {props.button_text} </button>
+        <button> {this.props.button_text} </button>
       </li>
     )
   }
